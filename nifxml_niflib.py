e75cb2bda8219df60000e5f1c393b18d64435605
Amorilia <amorilia@users.sourceforge.net>
2006-06-11 15:06:05 +0000

    Added license text to the nifxml_niflib.py file plus a brief description of the command line options.

--- nifxml_niflib.py
+++ nifxml_niflib.py
@@ -1,11 +1,52 @@
-"""
-This module generates C++ code for Niflib from the NIF file format specification XML.
-Created by Amorilia in 2006.
-@var ROOT_DIR:  Destination directory to write the generated code to.
-@type ROOT_DIR: string
-@var BOOTSTRAP: If true, this will generate the templates (the code that is not updated from the database). Specify "-b" on the command line to turn this on.
-@type BOOTSTRAP: bool
-"""
+# nifxml_niflib.py
+#
+# This script generates C++ code for Niflib.
+#
+# --------------------------------------------------------------------------
+# Command line options
+#
+# -p /path/to/niflib : specifies the path where niflib can be found 
+#
+# -b : enable bootstrap mode (generates templates)
+#
+# --------------------------------------------------------------------------
+# ***** BEGIN LICENSE BLOCK *****
+#
+# Copyright (c) 2005, NIF File Format Library and Tools
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+#    * Neither the name of the NIF File Format Library and Tools
+#      project nor the names of its contributors may be used to endorse
+#      or promote products derived from this software without specific
+#      prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ***** END LICENCE BLOCK *****
+# --------------------------------------------------------------------------
 
 from nifxml import *
 from distutils.dir_util import mkpath
