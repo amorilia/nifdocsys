dcb24376e40cc53c123b4dc890fc5d8968040643
Shon Ferguson <shonferg@users.sourceforge.net>
2007-04-01 19:54:23 +0000

    Changes to SWIG generation process: * GUID in project file now properly matches GUID in solution file. * Added option to specify destination path. * Made the default path: ../contrib/niflib/pywrap/swig * All projects are now generated by the script * Added final output reporting success and the location where the files were written. * Added generated file warning to all NiObject interface file that are created.

--- nifxml_swig.py
+++ nifxml_swig.py
@@ -3,20 +3,35 @@ from distutils.dir_util import mkpath
 import os
 import guid
 
+#Allow user to specify path where files will be generated with -p argument
+#By default, they will be generated in ../contrib/niflib/pywrap/swig
+ROOT_DIR = os.path.join('..', 'contrib', 'niflib', 'pywrap', 'swig' )
+
+prev = ""
+for i in sys.argv:
+    if prev == "-p":
+        ROOT_DIR = i
+    prev = i
+
 #Make sure output directory exists
-mkpath( os.path.join('.', 'swig') )
+mkpath( os.path.join(ROOT_DIR) )
 
 #Generate a project file, and an i file, and a bat file entry for each NiObject
 temp = Template()
 
-#Start bat file list with niflib
-temp.set_var("name", "niflib")
-bat_list = temp.parse( "templates/swig.bat.template" )
-
-#start solution file list with pyniflib
+#Start bat file list and solution file list with pyniflib
 temp.set_var("name", "pyniflib")
 temp.set_var( 'guid', guid.generate() )
-solution_list = temp.parse( "templates/pywrap.sln.proj.template" )
+bat_list = temp.parse( os.path.join('templates', 'swig.bat.template' ) )
+solution_list = temp.parse( os.path.join('templates', 'pywrap.sln.proj.template') )
+
+#Write pyniflib project
+f = file( os.path.join( ROOT_DIR, 'pyniflib.vcproj'), 'w' )
+f.write( temp.parse( os.path.join('templates', 'swig.vcproj.template' ) ) )
+f.close()
+
+#variable to report number of files generated
+files_generated = 1
 
 #Cycle through all NiObjects
 for n in block_names:
@@ -24,6 +39,9 @@ for n in block_names:
 
     #Mainly just the name is needed
     temp.set_var( "name", x.name )
+
+    #MSVC projects need a unique GUID as well
+    temp.set_var( 'guid', guid.generate() )
 
     #Create a list of ancestors to import
     ancestors = ""
@@ -34,32 +52,37 @@ for n in block_names:
     temp.set_var( "import_ancestors", ancestors )
 
     #Write project and interface files
-    f = file('./swig/' + x.name + '.vcproj', 'w')
-    f.write( temp.parse( "templates/swig.vcproj.template" ) )
+    f = file( os.path.join( ROOT_DIR, x.name + '.vcproj'), 'w' )
+    f.write( temp.parse( os.path.join('templates', 'swig.vcproj.template') ) )
     f.close()
+    files_generated += 1
 
-    f = file('./swig/' + x.name + '.i', 'w')
-    f.write( temp.parse( "templates/swig_niobject.i.template" ) )
+    f = file( os.path.join( ROOT_DIR, x.name + '.i'), 'w' )
+    f.write( temp.parse( os.path.join('templates', 'swig_niobject.i.template') ) )
     f.close()
+    files_generated += 1
 
     #append a row to the bat file
-    bat_list += temp.parse( "templates/swig.bat.template" )
+    bat_list += temp.parse( os.path.join('templates', 'swig.bat.template') )
 
     #apend a project to the solution file
-    temp.set_var( 'guid', guid.generate() )
-    solution_list += temp.parse( "templates/pywrap.sln.proj.template" )
+    solution_list += temp.parse( os.path.join('templates', 'pywrap.sln.proj.template') )
 
 #add "pause" to the end of bat file to keep window open
 bat_list += "pause"
 
 #Write bat file
-f = file('./swig/run_swig.bat', 'w')
+f = file( os.path.join( ROOT_DIR, 'run_swig.bat'), 'w' )
 f.write( bat_list )
 f.close()
+files_generated += 1
 
 #Write solution file
-f = file('./swig/pywrap.sln', 'w')
+f = file( os.path.join( ROOT_DIR, 'pywrap.sln'), 'w' )
 temp.set_var( 'projects', solution_list )
-result = temp.parse('templates/pywrap.sln.template')
+result = temp.parse(os.path.join('templates', 'pywrap.sln.template') )
 f.write( result )
 f.close()
+files_generated += 1
+
+print "nifxml_swig.py:  %d files successfully generated in %s"%(files_generated, ROOT_DIR)
